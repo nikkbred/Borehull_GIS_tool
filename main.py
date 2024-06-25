f71b2a109361569370e8576718d7fe331fb2ecac
niklas.brede <niklasbrede@outlook.com>
2024-06-25 13:58:17 +0200

    Update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import arcpy
 import pandas as pd
 import pathlib
+import os
 
 aprx = arcpy.mp.ArcGISProject('CURRENT')
 project_name = pathlib.Path(aprx.filePath)
@@ -38,9 +39,11 @@ for sheet_name in xls.sheet_names:
 
     # Read and process the data
     df = pd.read_excel(excel, sheet_name=sheet_name, skiprows=3)
-    df.columns = ['Dybde', 'Materiale', 'Tilstand']
-    df = df.iloc[1:].reset_index(drop=True)
+    df = df.dropna(axis=1, how='all')
+    df.columns = df.iloc[0]
+    df = df[1:].reset_index(drop=True)
     dfs[sheet_name] = df
+    arcpy.AddMessage(df)
 
     # Set local variables
     out_path = os.path.join(directory, 'Borehull')
@@ -61,26 +64,24 @@ for sheet_name in xls.sheet_names:
                                         config_keyword, spatial_grid_1, spatial_grid_2, spatial_grid_3, out_alias,
                                         oid_type)
     # Add fields to the feature class
-    arcpy.AddField_management(f"{out_path}/{out_name}", 'Dybde', 'DOUBLE')
-    arcpy.AddField_management(f"{out_path}/{out_name}", 'Materiale', 'TEXT', field_length=255)
-    arcpy.AddField_management(f"{out_path}/{out_name}", 'Tilstand', 'TEXT', field_length=255)
-    arcpy.AddField_management(f"{out_path}/{out_name}", 'Str', 'DOUBLE')
+    for col in df.columns:
+        if col.strip():  # Ensure the column name is not empty
+            if df[col].dtype == 'object':
+                field_type = 'TEXT'
+                arcpy.AddField_management(f"{out_path}/{out_name}", col, field_type, field_length=255)
+            elif df[col].dtype in ['int64', 'float64']:
+                field_type = 'DOUBLE'
+                arcpy.AddField_management(f"{out_path}/{out_name}", col, field_type)
     feature_class = f"{out_path}/{out_name}"
     borehole_location_data = [extract_coords(sheet_name)[1], extract_coords(sheet_name)[0], out_name]
     borehole_data = df
-    with arcpy.da.InsertCursor(f"{out_path}/{out_name}",
-                               ['SHAPE@XY', 'Dybde', 'Materiale', 'Tilstand', 'Str']) as cursor:
+    with arcpy.da.InsertCursor(f"{out_path}/{out_name}", ['SHAPE@XY'] + list(df.columns)) as cursor:
         for index, row in df.iterrows():
             xy = (extract_coords(sheet_name)[1], extract_coords(sheet_name)[0])
-            size = row['Dybde'] / 100
-            cursor.insertRow([xy, row['Dybde'], row['Materiale'], row['Tilstand'], size])
+            cursor.insertRow([xy] + list(row))
         del cursor
 
 for i in range(count):
-    # input_fc =f"C:\\Users\\niklas.brede\\OneDrive - Asplan Viak\\Documents\\Prosjekter\\Hydro_skredmal\\Skredmal_GIS_NY\\Mal_skredfarevurdering.gdb\\BH{i+1}"
-    # output_fc = f"C:\\Users\\niklas.brede\\OneDrive - Asplan Viak\\Documents\\Prosjekter\\Hydro_skredmal\\Skredmal_GIS_NY\\temp_directory\\BH{i+1}"
-    # shutil.copy(input_fc, output_fc)
-    # print('File copied')
     map_obj = aprx.listMaps()[3]
     layer = map_obj.addDataFromPath(os.path.join(directory, f"Borehull\\BH{i + 1}.shp"))
 
